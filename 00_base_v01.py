82afe56bc4678c8e507e148ac0e256709ded7d25
abbyive <107890625+abbyive@users.noreply.github.com>
2022-08-05 10:24:28 +1200

    skeleton of base code

---
+++ 00_base_v01.py
@@ -0,0 +1,18 @@
+"""
+
+base component for multiple choice quiz
+V0 - skeleton setup
+
+author - Abby Ives
+CC AVI 2022
+
+"""
+
+# import libraries below this line ****************************************
+
+
+# all functions below this line ********************************************
+
+
+# print main routine below this line *************************************
+print("hello")
